111bb0c6baa99446762bb2a5b8f3d659d75c7c62
VABIII <tbengtson82@gmail.com>
2022-01-22 09:16:31 -0600

    restricted auth-middleware.js built

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -9,8 +9,14 @@ const User = require('../users/users-model');
   }
 */
 function restricted(req, res, next) {
-    console.log('restricted');
-    next();
+    if (req.session.user) {
+        next()
+    } else {
+        next({
+            status: 401,
+            message: 'You shall not pass!'
+        })
+    }
 }
 /*
   If the username in req.body already exists in the database
@@ -49,6 +55,7 @@ async function checkUsernameExists(req, res, next) {
     try {
         const users = await User.findBy({username: req.body.username})
         if (users.length){
+            req.user = users[0]
             next();
         } else {
             next({
